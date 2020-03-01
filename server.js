72a6d1497c594b609dfd15fe3f08ad262632df21
1curtislee <1curtislee@gmail.com>
2020-02-29 17:45:54 -0800

    testing first slash command

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-const mongoose = require('mongoose');
+require('dotenv').config();
 const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
+const request = require('request')
 
-const API_PORT = process.env.PORT || 3001;
+const API_PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 const router = express.Router();
 
-// this is our MongoDB database
-const dbRoute =
-  'mongodb://heroku_81hwr9dc:b4h84mp6gl3n7enkpdgiko35qu@ds133252.mlab.com:33252/heroku_81hwr9dc'
-
-// connects our back end code with the database
-mongoose.connect(dbRoute, { 
-  useNewUrlParser: true,
-  useUnifiedTopology: true });
-let db = mongoose.connection;
-
-db.once('open', () => console.log('connected to the database'));
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.get('/get', (req, res) => {
+// router.get('/get', (req, res) => {
   
-});
+// });
 
-// create new collection:
-router.post('/post', (req, res) => {
-  
-});
-
+app.post('/', (req, res) => {
+  var data = {form: {
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: "#general",
+    text: "Hi! :wave: \n I'm your new bot."
+  }};
+  request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+        // Sends welcome message
+        res.json();
+      });
+  });
 
 // append /api for our http requests
 app.use('/api', router);
